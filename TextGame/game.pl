ffd6e77b73a27bcfb5eaf1aa2e1f138ee7137ccc
Preston Lamb <preston.j.lamb@gmail.com>
2013-03-28 21:56:01 -0600

    Beginning changes to 'have' and 'item'

--- TextGame/game.pl
+++ TextGame/game.pl
@@ -12,7 +12,13 @@
 :- use_module(library(readln)).
 
 % Use this dynamic fact to store the players current location
-:- dynamic current_area/1.	
+:- dynamic current_area/1.
+
+% Use this dynamic fact to add to the inventory
+:- dynamic have/1.
+
+% These are the items they can pick up
+item(key, 'Gold key to open doors').
 
 % Here is one way you might create your areas
 area(room1, 'Room 1', 'You are in Room 1').
