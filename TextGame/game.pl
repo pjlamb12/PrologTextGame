cdafc6defa3155d2f7aae14f74f920654f1b6c26
Preston Lamb <preston.j.lamb@gmail.com>
2013-04-08 18:43:22 -0600

    Finished everything except placing items inside the backpack. Waiting on Bryan's response
    One item to possibly add is requiring the flashlight to be turned on to
    find the idcard?

--- TextGame/game.pl
+++ TextGame/game.pl
@@ -21,7 +21,7 @@
 :- dynamic in_backpack/1.
 
 % Use this dynamic fact to unlock doors and other similar items
-:- dynamic unlocked/1.
+:- dynamic unblocked/1.
 
 % These are the items they can pick up
 item(key, 'Key', 'A key to open locked doors').
@@ -32,34 +32,34 @@ item(door, 'Door', 'This door stands between you and the final room').
 item(backpack, 'Backpack', 'The backpack will hold items that you pick up').
 item(elevator, 'Elevator', 'The elevator will take you to the top floor').
 item(candy, 'Snickers', 'The Snickers bar will give you extra energy').
-item(drink, 'Dr. Pepper', 'The Dr. Pepper will quench your thirst').
+item(soda, 'Dr. Pepper', 'The Dr. Pepper will quench your thirst').
 item(passage, 'Secret Passage', 'This passage will take you to the final room').
-item(money, 'Money', 'This gold coin is necessary to get in to the final room').
+item(money, 'Money', 'The money is necessary to get in to the final room').
 
 % Here is one way you might create your areas
-area(room1, 'The Registrar\'s Office', 'You are in Room 1').
-area(room2, 'The Ballroom', 'You are in Room 2').
-area(room3, 'The Bookstore', 'You are in Room 3').
-area(room4, 'The Marketplace', 'You are in Room 4').
-area(room5, 'The Hub', 'You have just entered the Hub, where many people are hanging out. You see some friends and go over to see what they\'re up to. After talking to them you look around for an item that will empty your hands a little bit and make walking around a little easier...').
+area(room1, 'The Registrar\'s Office', 'You just entered the Registrar\'s office. Nothing is really happening in this room.').
+area(room2, 'The Ballroom', 'You just entered the Ballroom. There are a lot of people milling about, and as you walk around you realize you\'ve dropped an important item that you use frequently on campus to prove who you are. Don\'t leave without finding it, you will not be able to finish your quest without it.').
+area(room3, 'The Bookstore', 'You just entered the Bookstore. You look around and see the aisles and aisles of school accessories, and realize that you may end up needing some sort of bag to take with you and store items.').
+area(room4, 'The Marketplace', 'You just entered the Marketplace. The lights are dim, and people have slipped in to chill out after the blaring music in the Sunburst Lounge. If you are going to move forward on the quest, however, you may want to look around for some light.').
+area(room5, 'The Hub', 'You have just entered the Hub, where many people are hanging out. You see some friends and go over to see what they\'re up to. After talking to them you see something on the ground that looks valuable. It may help you later when you get hungry. Also in the corner is an elevator that will take you up to the Sunburst Lounge.').
 area(room6, 'Sunburst Lounge', 'The Sunburst Lounge is bouncin. There is a live band playing up front, and hundreds of people milling about, making it really difficult to be able to find where the key you will need later in the quest is hidden.').
 area(room7, 'Quick Stop', 'You have entered the Quick Stop. All the partying and dancing has made you hungry. You step in here for a quick second to refuel.').
 area(room8, 'Admissions Office', 'You are in the Admissions office. You look around and see a lot of desks, but because it is after hours nobody is there working. Don\'t forget to leave without the rest of your costume.').
-area(room9, 'The Sky Room', 'You have reached the Sky Room!').
+area(room9, 'The Sky Room', 'You have reached the Sky Room! Congratulations on making it through the maze of rooms and for finding the key and the idcard. I hope you enjoyed playing \'Adventures at Utah State\'. Play again soon!').
 
 % Place the items in rooms
 placed(room1, passage).
-placed(room2, door).
+placed(room2, idcard).
 placed(room3, backpack).
 placed(room4, flashlight).
-placed(room4, drink).
-placed(room5, backpack).
+placed(room4, soda).
+placed(room5, money).
 placed(room6, key).
+placed(room6, door).
+placed(room5, elevator).
 placed(room7, candy).
 placed(room7, drink).
 placed(room8, mask).
-placed(room9, idcard).
-placed(room10, elevator).
 
 % What you can pick up and what you cant
 can_pickup(key).
@@ -72,6 +72,7 @@ can_pickup(mask).
 
 % Can eat an item
 can_eat(candy).
+can_eat(drink).
 
 % You might connect those areas like this:
 connected(south, room1, room4).
@@ -91,6 +92,25 @@ connected(east, room7, room8).
 connected(north, room8, room5).
 connected(north, room9, room6).
 
+% This blocks the final room from the one possible way to reach it.
+unblocked(room1, room4).
+unblocked(room1, room2).
+unblocked(room2, room3).
+unblocked(room2, room5).
+unblocked(room3, room2).
+unblocked(room3, room6).
+unblocked(room3, room6).
+unblocked(room4, room1).
+unblocked(room4, room7).
+unblocked(room5, room4).
+unblocked(room5, room8).
+unblocked(room5, room8).
+unblocked(room6, room3).
+unblocked(room7, room4).
+unblocked(room7, room8).
+unblocked(room8, room5).
+unblocked(room9, room6).
+
 % This rule starts everything off
 play :-
     retractall(current_area(_)),
@@ -98,7 +118,7 @@ play :-
     assertz(current_area(room8)),
     print_introduction,
     print_location,
-	dispPrompt,
+    dispPrompt,
     get_input.
 
 % Prints out a welcome and introduction
@@ -127,9 +147,27 @@ change_area(NewArea) :-
 dispPrompt :- prompt(_, '> ').
 
 % Add some help output here to explain how to play your game
-process_input([help]) :- print('Add some help output here...'), nl, nl.
+process_input([help]) :-
+    print('Here are some clues for how to play the game: '), nl, nl,
+    print('Type quest to start the game'), nl,
+    print('Type eat candy to eat the candy. You will lose the candy from your inventory after eating it.'), nl,
+    print('Type drink soda to drink a Dr. Pepper. You will lose the soda from your inventory after doing so.'), nl,
+    print('Type turnon flashlight to turn the flashlight on to light your way.'), nl,
+    print('Type open door to open a door. Make sure to have any items you will need to open the door first!'), nl,
+    print('Type inventory to see what you have in your inventory.'), nl,
+    print('Type contents to see what you have in your backpack.'), nl,
+    print('Type drop followed by an item name to drop the item from your inventory.'), nl,
+    print('Type fill_backpack followed by an item name to place the item in your backpack.'), nl,
+    print('Type pickup followed by an item name to pick up an item. Not all items are able to be picked up.'), nl, 
+    print('Type ride elevator to ride the elevator from the Hub to the Sunburst Lounge.'), nl,
+    print('Type go followed by a direction (north, south, east, west) to change rooms. Some rooms only allow you to come in to them, and if you try to go out a wall it will tell you and prevent you.'), nl, nl,
+    print('Hope these hints helped you! Good luck!'), nl, nl.
 
-% Input for eating the candy and any other food items
+% Display the description of the quest
+process_input([quest]):-
+    print('You have started the quest! Your mission, should you choose to accept, is to find two items and make it to the Sky Room. The Sky Room is located on the far side of the Sunburst Lounge. The first item that you need to find is the idcard. Good luck!').
+
+% Handling of the action 'eat _______'
 process_input([eat, candy]):-
     have(candy),
     retract(have(candy)),
@@ -137,14 +175,30 @@ process_input([eat, candy]):-
 process_input([eat, _]):-
     print('You must have the candy bar to eat it!'), nl, nl.
 
-% Opens doors and such if it is unlocked
+% Handling of the action 'drink _______'
+process_input([drink, soda]):-
+    have(soda),
+    retract(have(soda)),
+    print('You have drunk the Dr. Pepper. If you get thirsty, go back to the Quick Stop to get another!'), nl, nl.
+process_input([eat, _]):-
+    print('You must have the candy bar to eat it!'), nl, nl.
+
+% Handling of the action 'turnon _______'
+process_input([turnon, flashlight]):-
+    have(flashlight),
+    print('You have turned on the flashlight! It will now light your way through the dark rooms.'), nl, nl.
+process_input([eat, _]):-
+    print('You must have the flashlight to turn it on. Nice try though!'), nl, nl.
+
+% Handling of the action 'open _______'
 process_input([open, Item]):-
     have(key),
-    unlocked(Item),
-    retract(have(key)),
-    print('You have opened the door!'), nl, nl.
+    have(idcard),
+    current_area(room6),
+    print('You have opened the door!'), nl, nl,
+    change_area(room9).
 process_input([open, _]):-
-    print('You can\'t open the door, it is still locked.'), nl, nl.
+    print('You can\'t open the door, it is still locked. Make sure you have all the items you need and are in the correct room.'), nl, nl.
 
 % Print out a players inventory
 process_input([inventory]) :-
@@ -165,7 +219,7 @@ process_input([drop, Item]):-
 process_input([drop, _]):-
     print('You have nothing in your inventory by that name!'), nl, nl.
 
-% Place items in your backpack
+% Handling of the action 'fill_backpack ________'
 process_input([fill_backpack, Item]):-
     have(backpack),
     have(Item),
@@ -175,7 +229,22 @@ process_input([fill_backpack, Item]):-
 process_input([fill_backpack, _]):-
     print('You either don\'t have the backpack or have nothing to put in it!'), nl, nl.
 
-% Handling of the action 'pickup _______'
+% Handling of the action 'pickup idcard' and 'pickup key', and then 'pickup ______'
+process_input([pickup, idcard]):-
+    current_area(Current),
+    placed(Current, idcard),
+    assertz(have(idcard)),
+    item(idcard, ItemName, Description),
+    print('You picked up the '), write(ItemName), print('. Description: '), print(Description), nl, nl,
+    print('You have finished the first quest. Now go on to the next quest!'), nl, nl.
+process_input([pickup, key]):-
+    have(idcard),
+    current_area(Current),
+    placed(Current, key),
+    assertz(have(key)),
+    item(key, ItemName, Description),
+    print('You picked up the '), write(ItemName), print('. Description: '), print(Description), nl, nl,
+    print('You have finished the second quest. Now go finish the game by finding the Sky Room!'), nl, nl.
 process_input([pickup, Item]):-
     current_area(Current),
     placed(Current, Item),
@@ -185,13 +254,22 @@ process_input([pickup, Item]):-
 process_input([pickup, _]) :-
     print('There is nothing to pick up with that name. Sorry!'), nl, nl.
 
+% Ride the elevator
+process_input([ride, elevator]):-
+    current_area(room5),
+    print('You have used the elevator. Have fun!'), nl, nl,
+    change_area(room6).
+process_input([ride, _]):-
+    print('The elevator is not in this room. Sorry!'), nl, nl.
+
 % Handling of the action 'go _______', and a good example of how you might implement others
 process_input([go, Direction]) :-
     current_area(Current),
     connected(Direction, Current, NewRoom),
+    unblocked(Current, NewRoom),
     change_area(NewRoom).
 process_input([go, _]) :-
-    print('You hit an invisible wall and can\'t go that way'), nl, nl.
+    print('You can not go that way. You have either hit a wall, or there is something like a door blocking your way.'), nl, nl.
 process_input([_]) :-
     print('No idea what you are talking about...try again'), nl, nl.
 
